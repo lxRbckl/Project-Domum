b264e422ff855d5ec0f11b2707f7be30c5a3e58d
adrienhighlander <alexarbuckle@protonmail.com>
2021-07-16 16:29:10 -0500

    redundant security removed all functions formatted

--- Domum.py
+++ Domum.py
@@ -1,7 +1,7 @@
 # Project Domum by Alex Arbuckle
 
 
-import RPi.GPIO as GPIO
+#mport RPi.GPIO as GPIO
 from asyncio import sleep
 from discord import Intents
 from json import load, dump
@@ -15,24 +15,24 @@ token = ''
 
 @domum.event
 async def on_ready():
-        
-    GPIO.setmode(GPIO.BOARD)
+            
+    #GPIO.setmode(GPIO.BOARD)
     dictVariable = await jsonLoad()
-    for value in dictVariable['Data'].values():
+    #for value in dictVariable.values():
         
-        GPIO.setup(value[1], True if (value[0] == 'On') else (False))
-    
+        #GPIO.setup(value['Pin'], True if (value[0] == 'On') else (False))
+
     while (True):
         
-        for value in dictVariable['Data'].values():
+        for value in dictVariable.values():
             
-            GPIO.output(value[1], True if (value[0] == 'On') else (False))
-            
-            dictVariable = await jsonLoad()
-            await sleep(30)
+            #GPIO.output(value['Pin'], True if (value[0] == 'On') else (False))
+    
+        dictVariable = await jsonLoad()
+        await sleep(30)
 
 
-async def jsonLoad():
+async def jsonLoad(): # *
     '''  '''
 
     with open('Domum.json', 'r') as fileVariable:
@@ -40,7 +40,7 @@ async def jsonLoad():
         return load(fileVariable)
 
 
-async def jsonDump(arg):
+async def jsonDump(arg): # *
     ''' arg : dict '''
 
     with open('Domum.json', 'w') as fileVariable:
@@ -48,87 +48,64 @@ async def jsonDump(arg):
         dump(arg, fileVariable, indent=4)
 
 
-@domum.command()
-async def domumAddAdmin(ctx, arg):
-    ''' arg : str '''
-
-    dictVariable = await jsonLoad()
-    
-    if (str(ctx.author) in dictVariable['Admin']):
-    
-        dictVariable['Admin'].append(arg) if (arg not in dictVariable['Admin']) else (None)
-        
-        await jsonDump(dictVariable)
-    
-
-@domum.command()
-async def domumRemoveAdmin(ctx, arg):
-    ''' arg : str '''
-   
-    dictVariable = await jsonLoad()
-    
-    if (str(ctx.author) in dictVariable['Admin']):
-    
-        dictVariable['Admin'].remove(arg) if (arg in dictVariable['Admin']) else (None)
-        
-        await jsonDump(dictVariable)
-
-
-@domum.command()
-async def domumShowAdmin(ctx):
-    '''  '''
-    
-    dictVariable = await jsonLoad()
-    
-    if (str(ctx.author) in dictVariable['Admin']):
-    
-        strVariable = ''.join('{}\n'.format(i) for i in dictVariable['Admin'])
-        
-        await ctx.channel.send(strVariable, delete_after = 60)
-    
-
-@domum.command(aliases = ['add', 'Add'])
+@domum.command(aliases = ['add', 'Add']) # *
 async def domumAdd(ctx, *args):
     ''' args[0] : str
         args[1] : int '''
     
     dictVariable = await jsonLoad()
     
-    if (str(ctx.author) in dictVariable['Admin']):
-    
-        dictVariable[args[0]] = ['Off', args[1], False]
+    if (args[0] not in dictVariable.keys()):
+        
+        dictVariable[args[0]] = {'Status' : 'Off',
+                                 'Schedule' : False,
+                                 'Pin' : int(args[1])}
         
         await jsonDump(dictVariable)
-        await ctx.channel.send('{} was added.'.format(args[0]), delete_after = 60)    
+        await ctx.channel.send('{} was added.'.format(args[0]), delete_after = 60)
+    
+    else:
+        
+        await ctx.channel.send('{} already exists.'.format(args[0]), delete_after = 60)
+    
 
-
-@domum.command(aliases = ['delete', 'Delete'])
+@domum.command(aliases = ['delete', 'Delete']) # *
 async def domumDelete(ctx, arg):
     ''' arg : str '''
     
     dictVariable = await jsonLoad()
     
-    if (str(ctx.author) in dictVariable['Admin']):
+    if (arg in dictVariable.keys()):
         
         del dictVariable[arg]
         
         await jsonDump(dictVariable)
         await ctx.channel.send('{} was removed.'.format(arg), delete_after = 60)
+    
+    else:
+        
+        await ctx.channel.send('{} does not exist.'.format(arg), delete_after = 60)
+        
 
-
-@domum.command(aliases = ['set', 'Set'])
+@domum.command(aliases = ['set', 'Set']) # *
 async def domumSet(ctx, *args):
-    ''' args[n] : str '''
+    ''' args[n] : str
+        args[-1] : str '''
     
     dictVariable = await jsonLoad()
     
-    if (str(ctx.author) in dictVariable['Admin']):
-
-        for arg in [i for i in dictVariable.keys()] if (args[1:] == []) else args[1:]:
+    for arg in [i for i in dictVariable.keys()] if (args[1:] == []) else (args[1:]):
         
-            dictVariable[arg][0] = args[0].title() if (arg in dictVariable.keys()) else (None)
+        if (arg in dictVariable.keys()):
         
-        await jsonDump(dictVariable)
-
+            dictVariable[arg]['Status'] = args[-1].title()
+            
+            await jsonDump(dictVariable)
+            await ctx.channel.send('{} : Status : {}'.format(arg, args[-1].title), delete_after = 60)
+            
+        else:
+            
+            await ctx.channel.send('{} does not exist'.format(arg), delete_after = 60)
+        
 
 domum.run(token)
